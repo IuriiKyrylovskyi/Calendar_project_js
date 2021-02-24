4cf1e9220002cd169c02599812b36e06b6c1b8f6
IuriiKyrylovskyi <iurii712@ukr.net>
2021-02-24 14:04:01 +0200

    navig curr_month

--- scripts/header/navigation.js
+++ scripts/header/navigation.js
@@ -79,6 +79,6 @@ const onChangeWeek = event => {
 };
 
 export const initNavigation = () => {
-  // renderCurrentMonth();
+  renderCurrentMonth();
   navElem.addEventListener('click', onChangeWeek);
 };
